16e9d29273b1a7c79d9f95325af01d2e5e2ff7e2
Ankit6055 <ankit.kumarrrrr6055@gmail.com>
2025-05-08 18:29:38 +0530

    Update: Auth Controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,17 +1,95 @@
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 export const registerUser = async (req, res) => {
-  const {name, email, password} = req.body;
+  const { name, email, password, phone } = req.body;
 
-  if (!name || !email || !password) {
+  if (!name || !email || !password || !phone) {
     console.log("Data is missing");
     return res.status(400).json({
       success: false,
-      message: "All fields are required"
-    })
+      message: "All fields are required",
+    });
   }
 
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: { email },
+    });
 
-}
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    // hash the password
+    const hashedPassword = bcrypt.hash(password, 10);
+    const verificationToken = crypto.randomBytes(32).toString("hex");
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        phone,
+        password: hashedPassword,
+        verificationToken,
+      },
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error,
+      message: "Registration failed",
+    });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.send(400).json({
+      success: false,
+      message: "All fields are requires",
+    });
+  }
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return res.send(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.send(400).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = jwt.sign(
+      {id: user.id, role: user.role},
+      process.env.JWT_SECRET,
+      {expiresIn: '24h'}
+    )
+
+  } catch (error) {
+    return res.send(400).json({
+      success: false,
+      message: "Login failed",
+    });
+  }
+};
